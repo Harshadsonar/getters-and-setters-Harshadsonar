9298907d879c5db16ea42aa2de51b8b94f124475
Harshadsonar <sonarharshad02@gmail.com>
2023-07-14 17:19:52 +0530

    solved

--- script.js
+++ script.js
@@ -26,16 +26,7 @@ class Teacher extends Person {
         console.log(`${this._name} is teaching.`);
     }
 }
-const john = new Person('John', 25);
-console.log(john.name); // Output: John
-john.age = 30;
-console.log(john.age); // Output: 30
 
-const alice = new Student('Alice', 20);
-alice.study(); // Output: Alice is studying.
-
-const mrSmith = new Teacher('Mr. Smith', 35);
-mrSmith.teach(); // Output: Mr. Smith is teaching.
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;

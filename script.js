8e9b0a9068482a833c31c27a5e50d721b55b1bbd
Harshadsonar <sonarharshad02@gmail.com>
2023-07-14 17:18:13 +0530

    solved

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    get name() {
+        return this._name;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    study() {
+        console.log(`${this._name} is studying.`);
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+    teach() {
+        console.log(`${this._name} is teaching.`);
+    }
+}
+const john = new Person('John', 25);
+console.log(john.name); // Output: John
+john.age = 30;
+console.log(john.age); // Output: 30
 
+const alice = new Student('Alice', 20);
+alice.study(); // Output: Alice is studying.
+
+const mrSmith = new Teacher('Mr. Smith', 35);
+mrSmith.teach(); // Output: Mr. Smith is teaching.
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
